0805c79420d4244fe5b96027f8b0a861d9e118db
Filipe Morelli <morellitecinfo@gmail.com>
2016-03-18 17:01:22 -0300

    Alterações arquivos Primarios

---
+++ js/my-app.js
@@ -0,0 +1,16 @@
+// Initialize your app
+var myApp = new Framework7({
+    material: true,
+    pushState: true,
+    swipePanel: 'left',
+    swipePanelActiveArea: 56
+});
+
+// Export selectors engine
+var $ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
