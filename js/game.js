1095ca5719f219d18c7f85d75cad5195dbac513a
Filipe Morelli <morellitecinfo@gmail.com>
2016-03-21 11:33:47 -0300

    Ajustes Modo Humano

--- js/game.js
+++ js/game.js
@@ -5,43 +5,43 @@ var Game; //variavel global que contem o jogo
     var $window = $(window); // variavel global window
     //objeto Game contem todo o jogo
     Game = {
-        mosquito: {
-            paginaModoMosquito: null, //contem a pagina que acontecerá o jogo não posso instancia para receber a pagina já que ainda não foi carregada no DOM
-            start: function (page) {
-                this.paginaModoMosquito = page;
-
-                setInterval(function () {
-                    //exemplo utilizando o gerador de mosquito
-                    var mosquito = $('<img class="mosquito" src="img/mosquito.png" width="36" height="36">');
-                    mosquito.css("top", Game.randomTop(mosquito));
-                    mosquito.css("left", Game.randomLeft(mosquito));
-                    mosquito.on("mousedown touchstart", function (e) {
-                        e.preventDefault();
-                        console.log("Removeu")
-                        $(this).remove();
-                    });
-                    Game.mosquito.paginaModoMosquito.append(mosquito);
-                }, 5000)
-            },
-        },
         humano: {
-            paginaModoHumano: null,
+            paginaModoHumano: null, //contem a pagina que acontecerá o jogo não posso instancia para receber a pagina já que ainda não foi carregada no DOM
             start: function (page) {
                 this.paginaModoHumano = page;
+                this.gerarMosquito();
+                setInterval(this.gerarMosquito, 5000)
+            },
+            gerarMosquito: function () {
+                var mosquito = $('<img class="mosquito" src="img/mosquito.png" width="36" height="36">');
+                mosquito.css("top", Game.randomTop(mosquito));
+                mosquito.css("left", Game.randomLeft(mosquito));
+                mosquito.on("mousedown touchstart", function (e) {
+                    e.preventDefault();
+                    console.log("Removeu")
+                    $(this).remove();
+                });
+                Game.humano.paginaModoHumano.append(mosquito);
+            }
+        },
+        mosquito: {
+            paginaModoMosquito: null,
+            start: function (page) {
+                this.paginaModoMosquito = page;
             }
         },
         //objetos globais do Game tanto para o modo mosquito e para modo humano
         //retirar 36 pois ele é o tamanho fixo da imagem mosquitp
-        randomTop: function (mosquitoObjeto) {
+        randomTop: function (obj) {
             var aleatorio = Math.random() * $window.height();
             if (aleatorio + 36 >= $window.height())
-                return $window.height() - mosquitoObjeto.height() + "px";
+                return $window.height() - obj.height() + "px";
             return aleatorio + "px";
         },
-        randomLeft: function (mosquitoObjeto) {
+        randomLeft: function (obj) {
             var aleatorio = Math.random() * $window.width();
             if (aleatorio + 36 >= $window.width())
-                return $window.width() - mosquitoObjeto.width() + "px";
+                return $window.width() - obj.width() + "px";
             return aleatorio + "px";
         },
     }
--- js/paginas.js
+++ js/paginas.js
@@ -1,7 +1,11 @@
 (function (window, document, navigato, myApp, $, DataBase, Game) {
 
+    myApp.onPageInit('modo-humano', function (page) {
+        Game.humano.start($(".modo-humano"));
+    });
+    
     myApp.onPageInit('modo-mosquito', function (page) {
-        Game.mosquito.start($(".modo-mosquito"));
+        //Game.mosquito.start($(".modo-mosquito"));
     });
 
 })(window, document, navigator, myApp, Dom7, DataBase, Game)
